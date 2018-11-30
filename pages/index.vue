6aa9ec416442b0d2c07daabad4b6c92452604061
cherish <591981210@qq.com>
2018-11-30 13:25:42 +0800

    异步数据获取

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,16 @@
 <template>
   <section class="container">
+    <!-- nuxt-link组件和router-link组件的作用一样，都能用来做路由跳转 -->
     <nuxt-link to="/student">跳转到student页面</nuxt-link>
-    <button @click="jumpTo">跳转到 student 页面</button>
-    <!--a 标签相当于刷新页面-->
-    <!--<a href="/student">跳转到 student 页面</a>-->
+    <button @click="jumpTo">跳转到student页面</button>
+    <!-- 跳转不能直接写a标签，因为a标签相当于重新刷新页面 -->
+    <!-- <a href="/student">跳转到student页面</a> -->
     <el-button>测试el-button</el-button>
+    <ul>
+      <li
+        v-for="item in movieList"
+        :key="item.id">{{ item.title }}</li>
+    </ul>
   </section>
 </template>
 
@@ -15,10 +21,66 @@ export default {
   components: {
     Logo
   },
-  methods:{
-    jumpTo(){
-      // this.$router.push({path:'/student'})
-      this.$router.push({name:'student'})
+  data () {
+    return {
+      movieList: [
+        // {title: '肖申克的救赎', id: 1},
+        // {title: '大圣归来', id: 2},
+        // {title: '当幸福来敲门', id: 3}
+      ]
+    }
+  },
+  // 在nuxt中，生命周期函数只有created以及beforeCreate这两个生命周期函数能够在服务端正常使用
+  // 在nuxt中发异步请求不能在created生命周期函数中去发，因为它会在前端执行
+  created () {
+    // console.log('created');
+    // setTimeout(() => {
+    //   this.movieList = [
+    //     {title: '肖申克的救赎', id: 1},
+    //     {title: '大圣归来', id: 2},
+    //     {title: '当幸福来敲门', id: 3}
+    //   ]
+    // }, 1000);
+  },
+  // 发送异步请求，在asyncData函数中发送,服务器中发送请求
+  // asyncData(context, callback) {
+    // console.log(context);
+    // setTimeout(() => {
+    //   // callback有两个参数，第一个是错误对象，第二个是数据对象
+    //   callback(null, {
+    //     movieList: [
+    //       {title: '肖申克的救赎1', id: 1},
+    //       {title: '大圣归来1', id: 2},
+    //       {title: '当幸福来敲门1', id: 3}
+    //     ]
+    //   })
+    // }, 1000);
+  // },
+  asyncData(context, callback) {
+    console.log(111)
+    console.log(context.route);
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({
+          movieList: [{title: '肖申克的救赎2', id: 1},
+        {title: '大圣归来2', id: 2},
+        {title: '当幸福来敲门2', id: 3}]
+        })
+      }, 1000);
+    })
+      .then(data => {
+        console.log(data);
+        callback(null, {movieList: data.movieList})
+      })
+      .catch(err => {
+        callback(err)
+        context.error(err)
+      })
+  },
+  methods: {
+    jumpTo() {
+      // this.$router.push({path: '/student'})
+      this.$router.push({name: 'student'})
     }
   }
 }
@@ -26,33 +88,4 @@ export default {
 
 <style>
 
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
