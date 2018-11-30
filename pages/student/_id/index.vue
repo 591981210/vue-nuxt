a12225aa5204dbc2aa89906ab810f63bc264fb5b
cherish <591981210@qq.com>
2018-11-30 12:41:15 +0800

    动态路由+路由参数校验

---
+++ pages/student/_id/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="stu-detail">
+    <!-- 这个id对应一个加了下划线的文件夹名 -->
+    这是{{ $route.params.id }}号学生的详细信息
+  </div>
+</template>
+<script>
+export default {
+  // nuxt中使用validate方法进行路由参数校验，这个方法必须返回一个布尔值，为true表示校验通过，为false表示校验失败。注意validate不能写到methods属性中。
+  validate(obj) {
+    // console.log(obj);
+    // return true
+    //  只有数字 id
+    return /^\d+$/.test(obj.params.id)
+  }
+}
+</script>
--- pages/student/index.vue
+++ pages/student/index.vue
@@ -1,14 +1,26 @@
 <template>
-  <div>
-    这是一个学生页面
+  <div class="student">
+    <h3>这是学生列表页</h3>
+    <ul>
+      <li
+        v-for="item in stuList"
+        :key="item.id">
+        <nuxt-link :to="`/student/${item.id}`">{{ item.name }}</nuxt-link>
+      </li>
+    </ul>
   </div>
 </template>
-
 <script>
   export default {
+    data () {
+      return {
+        stuList: [
+          {name: 'jack1', id: 1},
+          {name: 'jack2', id: 2},
+          {name: 'jack3', id: 3}
+        ]
+      }
+    }
   }
 </script>
 
-<style scoped>
-
-</style>
