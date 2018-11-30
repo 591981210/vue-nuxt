0faf09e6f67e91cba96d6c9428e709d8e5952b4f
cherish <591981210@qq.com>
2018-11-30 12:49:00 +0800

    嵌套路由

---
+++ pages/teacher.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="teacher">
+    <h3>这是教师列表页1</h3>
+    <ul>
+      <li
+        v-for="item in teacherList"
+        :key="item.id">
+        <nuxt-link :to="`/teacher/${item.id}`">{{ item.name }}</nuxt-link>
+      </li>
+    </ul>
+    <!-- nuxt-child组件用来存放嵌套的组件 -->
+    <nuxt-child/>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      teacherList: [
+        {name: 'jack1', id: 1},
+        {name: 'jack2', id: 2},
+        {name: 'jack3', id: 3}
+      ]
+    }
+  }
+}
+</script>
+
